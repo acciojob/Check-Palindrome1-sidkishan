773ab24ea19481f8b90c3c7dc40d43b5925abb17
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-03-18 16:39:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 // complete the given function
 
 function palindrome(str){
-
+let i = 0;
+	let j = str.length() - 1;
+	while(i < j){
+		if(str.charAt(i) ==  ' '){
+			i++;
+		}
+		else if(str.charAt(j) ==  ' '){
+			j--;
+		}
+		else if(str.charAt(i) === str.charAt(j)){
+			i++;
+			j--;
+		}
+		else{
+			return false;
+		}
+	}
+	return true;
 }
 module.exports = palindrome
